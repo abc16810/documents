fd2c615adbb501ebfef36f51f106bb4e6e11aaee
abc16810 <1959166627@qq.com>
2025-01-23 09:09:14 +0800

    first commit

---
+++ 机器&深度学习/深度学习练习/work4.py
@@ -0,0 +1,156 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import paddle
+import paddle.nn.functional as F
+from paddle.nn import Conv2D, Linear, MaxPool2D
+from paddle.vision.transforms import Normalize
+
+
+def get_MNIST_dataloader():
+    # 定义图像归一化处理方法，这里的CHW指图像格式需为 [C通道数，H图像高度，W图像宽度]
+    transform = Normalize(mean=[127.5], std=[127.5], data_format='CHW')
+    # 下载数据集并初始化 DataSet
+    train_dataset = paddle.vision.datasets.MNIST(mode='train', transform=transform)
+    test_dataset = paddle.vision.datasets.MNIST(mode='test', transform=transform)
+
+    # 定义并初始化数据读取器
+    train_loader = paddle.io.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=1, drop_last=True)
+    test_loader = paddle.io.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=1, drop_last=False)
+
+    return train_loader, test_loader
+
+    
+class MNIST(paddle.nn.Layer):
+     def __init__(self):
+         super(MNIST, self).__init__()
+         
+         # 定义卷积层，输出特征通道out_channels设置为20，卷积核的大小kernel_size为5，卷积步长stride=1，padding=2
+         # 输出的宽和高不变
+         self.conv1 = Conv2D(in_channels=1, out_channels=20, kernel_size=5, stride=1, padding=2)
+         # 定义池化层，池化核的大小kernel_size为2，池化步长为2
+         self.max_pool1 = MaxPool2D(kernel_size=2, stride=2)
+         # 定义卷积层，输出特征通道out_channels设置为20，卷积核的大小kernel_size为5，卷积步长stride=1，padding=2
+         self.conv2 = Conv2D(in_channels=20, out_channels=20, kernel_size=5, stride=1, padding=2)
+         # 定义池化层，池化核的大小kernel_size为2，池化步长为2
+         self.max_pool2 = MaxPool2D(kernel_size=2, stride=2)
+         # 定义一层全连接层，输出维度是1
+         self.fc = Linear(in_features=980, out_features=10)
+         
+    # 定义网络前向计算过程，卷积后紧接着使用池化层，最后使用全连接层计算最终输出
+    # 卷积层激活函数使用Relu，全连接层不使用激活函数
+     def forward(self, inputs):
+         x = self.conv1(inputs)
+         x = F.relu(x)
+         x = self.max_pool1(x)
+         x = self.conv2(x)
+         x = F.relu(x)
+         x = self.max_pool2(x)
+         x = paddle.reshape(x, [x.shape[0], -1])
+         x = self.fc(x)
+         return x
+
+
+train_loader, val_loader = get_MNIST_dataloader()
+
+
+def evaluation(model, datasets):
+    model.eval()
+
+    acc_set = list()
+    for batch_id, data in enumerate(datasets()):
+        images, labels = data
+        images = paddle.to_tensor(images)
+        labels = paddle.to_tensor(labels)
+        pred = model(images)   # 获取预测值
+        acc = paddle.metric.accuracy(input=pred, label=labels)
+        acc_set.extend([acc.numpy()])
+    
+    # #计算多个batch的准确率
+    acc_val_mean = np.array(acc_set).mean()
+    return acc_val_mean
+
+
+#网络结构部分之后的代码，保持不变
+def train(model):
+    model.train()
+    
+    # 使用SGD优化器，learning_rate设置为0.01
+    opt = paddle.optimizer.SGD(learning_rate=0.001, parameters=model.parameters())
+    # 训练5轮
+    EPOCH_NUM = 10
+
+    IMG_ROWS, IMG_COLS = 28, 28
+    loss_list = []
+    
+    for epoch_id in range(EPOCH_NUM):
+        for batch_id, data in enumerate(train_loader()):
+            #准备数据
+            images, labels = data
+
+            images = paddle.to_tensor(images)
+
+            labels = paddle.to_tensor(labels)
+            
+            #前向计算的过程
+            predicts = model(images)
+            #计算损失，取一个批次样本损失的平均值
+            loss = F.cross_entropy(predicts, labels)
+            avg_loss = paddle.mean(loss)
+
+            #每训练200批次的数据，打印下当前Loss的情况
+            if batch_id % 200 == 0:
+                loss_list.append(avg_loss.numpy())
+                print("epoch: {}, batch: {}, loss is: {}".format(epoch_id, batch_id, avg_loss.numpy()))
+            
+            #后向传播，更新参数的过程
+            avg_loss.backward()
+            # 最小化loss,更新参数
+            opt.step()
+            # 清除梯度
+            opt.clear_grad()
+
+        acc_train_mean = evaluation(model, train_loader)
+        acc_val_mean = evaluation(model, val_loader)
+        print('train_acc: {}, val acc: {}'.format(acc_train_mean, acc_val_mean))  
+    #保存模型参数
+    paddle.save(model.state_dict(), 'mnist.pdparams')
+    return loss_list
+
+### 训练
+model = MNIST()
+#loss_list = train(model)
+
+
+
+
+### 验证
+
+from PIL import Image
+
+
+# 读取一张本地的样例图片，转变成模型输入的格式
+def load_image(img_path):
+    # 从img_path中读取图像，并转为灰度图
+    im = Image.open(img_path).convert('L')
+    im = im.resize((28, 28), 4)
+    im = np.array(im).reshape(1, 1, 28, 28).astype(np.float32)
+    # 图像归一化
+    im = 1.0 - im / 255.
+    return im
+
+
+params_file_path = 'mnist.pdparams'
+img_path = 'work/example_0.jpg'
+# 加载模型参数
+param_dict = paddle.load(params_file_path)
+model.load_dict(param_dict)
+# 灌入数据
+model.eval()
+tensor_img = load_image(img_path)
+#模型反馈10个分类标签的对应概率
+results = model(paddle.to_tensor(tensor_img))
+#取概率最大的标签作为预测输出
+lab = np.argsort(results.numpy())
+print("本次预测的数字是: ", lab[0][-1])
